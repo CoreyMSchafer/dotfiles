9f49fe8d9a321da552c3c98e060e3f653f760b75
Corey Schafer <CoreyMSchafer@gmail.com>
2016-02-14 21:55:31 -0700

    Changed Install Script Arguments

--- .bash_profile
+++ .bash_profile
@@ -1,5 +1,6 @@
 # Add `~/bin` to the `$PATH`
 export PATH="$HOME/bin:$PATH";
+export PATH="/usr/local/sbin:$PATH"
 
 # Load the shell dotfiles, and then some:
 # * ~/.path can be used to extend `$PATH`.
--- install.sh
+++ install.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="aliases bash_profile bash_prompt bashrc functions path private"    # list of files/folders to symlink in homedir
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+dir=${homedir}/dotfiles                    # dotfiles directory
+olddir=${homedir}/dotfiles_old             # old dotfiles backup directory
+files="aliases bash_profile bash_prompt bashrc functions path private"    # list of files/folders to symlink in ${homedir}
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > ${homedir}/.git-completion.bash
+
+# create dotfiles_old in ${homedir}
+echo "Creating ${olddir} for backup of any existing dotfiles in ${homedir}"
+mkdir -p ${olddir}
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the ${dir} directory"
+cd ${dir}
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+# move any existing dotfiles in ${homedir} to dotfiles_old directory, then create symlinks
+for file in ${files}; do
+    echo "Moving any existing dotfiles from ${homedir} to ${olddir}"
+    mv ${homedir}/.${file} ${homedir}/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
+    ln -s ${dir}/.${file} ${homedir}/.${file}
 done
