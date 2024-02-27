66632381c86541808b593009dd9caa3445cca281
Corey Schafer <CoreyMSchafer@gmail.com>
2024-02-27 05:23:22 -0500

    Commands weren't in PATH on first script execution. Manually loaded them at the top of each script.

--- sublime.sh
+++ sublime.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env zsh
+
+# Check if Homebrew's bin exists and if it's not already in the PATH
+if [ -x "/opt/homebrew/bin/brew" ] && [[ ":$PATH:" != *":/opt/homebrew/bin:"* ]]; then
+    export PATH="/opt/homebrew/bin:$PATH"
+fi
+
 # Check if 'subl' command is available
 if ! command -v subl &>/dev/null; then
     echo "'subl' command not found. Creating symlink for Sublime Text."
--- vscode.sh
+++ vscode.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+# Check if Homebrew's bin exists and if it's not already in the PATH
+if [ -x "/opt/homebrew/bin/brew" ] && [[ ":$PATH:" != *":/opt/homebrew/bin:"* ]]; then
+    export PATH="/opt/homebrew/bin:$PATH"
+fi
+
 # Install VS Code Extensions
 extensions=(
     ms-python.python
